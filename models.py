c8fbe8b12dfefc510fb3c40f5b3b48f876f097e2
JimmyTanUWA <165866845+JimmyTanUWA@users.noreply.github.com>
2024-05-16 12:00:41 +0800

    Added routing and setup files, and database models
    Three forms in forms.py, signupform, signinform and chatform, which are forms that should render into user posts. Currently only have two models, user and chat. Chat objects "belong" to User objects by storing the userid. auth.py and views.py is routing split into two. Auth specifically handles routes for authentication related, views are stuff that gets rendered after authentication.

---
+++ models.py
@@ -0,0 +1,45 @@
+from . import db
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(150))
+
+    def get_id(self):
+        return str(self.id)
+
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(1000))
+    img_path = db.Column(db.String(100), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('chats', lazy=True))
+
+
+
+""" 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(150))
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+ """
